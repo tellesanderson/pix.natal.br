b1c2751044717b017b7a6e36f39d1b65c59dfc30
tellesanderson <168309101+tellesanderson@users.noreply.github.com>
2024-05-18 18:10:51 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,8 +2,7 @@ const canvasNestConfig = {
     color: '0,255,0',
     count: 88,
     opacity: 0.7,
-    zIndex: -1,
-    zIndex_mode: 'behind'
+    zIndex: 2
 };
 
 new CanvasNest(document.getElementById('canvas-nest-container'), canvasNestConfig);
